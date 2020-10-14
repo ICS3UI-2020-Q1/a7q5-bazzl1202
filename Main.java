65f3ce97d0b8bed21a01127fa21f8a39c97d4e5a
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-14 12:16:49 +0000

    Now prints out first digit of a number

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
 /**
- *
- * @author 
+ * A method that prints out the first digit of a number
+ * @author Liana Bazzarella
  */
 public class Main {
+
+  // printsout first digit of a number
+  public static int firstDigit(int integer){
+    // makes it still work if the number is negative
+    if(integer < 10){
+      integer = -integer / 10;
+    }
+    while(integer > 10){
+      integer = integer / 10;
+    }
+    System.out.println(integer);
+    return integer;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // this is the number used
+    firstDigit(53718);
   }
 }
